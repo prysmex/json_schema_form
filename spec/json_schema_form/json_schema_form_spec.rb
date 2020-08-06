86e1f1b61b6943c73bfef150b78992e303b88cdb
Pato <pato_devilla@hotmail.com>
2020-08-06 02:37:21 -0500

    rename spec file

--- spec/json_schema_form/json_schema_form_spec.rb
+++ spec/json_schema_form/json_schema_form_spec.rb
@@ -1,6 +1,6 @@
 require 'json_schema_form'
 
-describe ::SuperHash::Hasher do
+describe ::JsonSchemaForm::Document::Document do
   # describe '.attribute' do
   #   it "can add a attribute" do
   #     described_class.attribute(:id)
