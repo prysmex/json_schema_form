eecc60379642fd436dc13f6ed209b5fb8b5a396f
Pato <pato_devilla@hotmail.com>
2021-05-11 19:15:17 -0500

    add methods to component

--- lib/json_schema_form/schema_form/field/component.rb
+++ lib/json_schema_form/schema_form/field/component.rb
@@ -30,6 +30,20 @@ module SchemaForm
         errors['$ref_path'] = "$ref must match this regex #{REF_REGEX}" if self[:$ref]&.match(REF_REGEX).nil?
         errors
       end
+
+      def component_definition_id
+        self.dig(*[:$ref])
+      end
+
+      def component_definition_id=(id)
+        SuperHash::Utils.bury(self, *[:$ref], "#/definitions/#{id}")
+      end
+
+      #get the field's response set, only applies to certain fields
+      def component_definition
+        path = self.component_definition_id&.sub('#/', '')&.split('/')&.map(&:to_sym)
+        root_parent&.dig(*path) unless path.nil? || path.empty?
+      end
   
     end
   end
