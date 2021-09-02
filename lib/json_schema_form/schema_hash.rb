3edfcab6a5d58a247f1461be9412b84457220097
Pato <pato_devilla@hotmail.com>
2021-09-02 13:27:14 -0500

    fix

--- lib/json_schema_form/schema_hash.rb
+++ lib/json_schema_form/schema_hash.rb
@@ -3,9 +3,4 @@ require 'super_hash'
 # base class
 class SchemaHash < Hash#ActiveSupport::HashWithIndifferentAccess
   include ::SuperHash::Hasher
-
-  def initialize(init_value=nil, options={})
-    super(init_value, options)
-  end
-
 end
